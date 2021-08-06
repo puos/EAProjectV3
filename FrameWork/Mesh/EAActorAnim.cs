5c1dd584223ded1c63b3973bc27abb0370b26932
puos033 <puos033@snowpipe.co.kr>
2021-08-06 18:06:23 +0900

    ActorAnim 수정

---
+++ FrameWork/Mesh/EAActorAnim.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public enum AnimationEventType
+{
+    None,
+    Impact,
+    Sfx,
+    PlaySound,
+}
+
+
+// animation management class
+public abstract class EAActorAnim : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex, controller);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex, controller);
+    }
+
+    //  animation event callback
+
+}
