9fe6c0e1512e34816dc503c535bb605713915836
puos033 <puos033@snowpipe.co.kr>
2021-08-12 18:15:14 +0900

    사운드 작업

--- Assets/Plugins/EAProjectV3/FrameWork/Sfx/EASoundManager.cs
+++ Assets/Plugins/EAProjectV3/FrameWork/Sfx/EASoundManager.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
+public enum EASOUND_TYPE { BGM , SFX, UI,VOICE, AMBIENT }
+
 public class EASoundManager : Singleton<EASoundManager>
 {
     public const string bgmVolume = "BgmVolume";
@@ -19,15 +21,79 @@ public class EASoundManager : Singleton<EASoundManager>
 
     AudioLowPassFilter lowPassFilter = null;
 
+    EABGMGroup bGMGroup = null;
+
+    // BGM , SFX volume
+    private Action<float, float> onChangeVolume;
+
     public override GameObject GetSingletonParent()
     {
         return EAMainFrame.instance.gameObject;
     }
-
     protected override void Initialize()
     {
         base.Initialize();
 
+        if(mainAudio == null)
+        {
+            mainAudio = gameObject.AddComponent<AudioSource>();
+            subAudio = gameObject.AddComponent<AudioSource>();
+            mainAudioOriVolume = mainAudio.volume;
+            mainAudio.volume = GetVolume(EASOUND_TYPE.BGM) * mainAudio.volume;
+        }
+
         EAMainFrame.instance.OnMainFrameFacilityCreated(MainFrameAddFlags.SoundMgr);
+    }
+
+    // Once the volume has been modified it can be called to call back
+    public void OnAddListner_OnChangeVolume(Action<float,float> action)
+    {
+        onChangeVolume -= action;
+        onChangeVolume += action;
+    }
+    public void RemoveListener_OnChangeVolume(Action<float,float> action)
+    {
+        onChangeVolume -= action;
+    }
+    public float GetVolume(EASOUND_TYPE type)
+    {
+        float result = 1;
+        if (type == EASOUND_TYPE.BGM) result = GetBGMVolumeNormalized();
+        if (type == EASOUND_TYPE.SFX) result = GetSFXVolumeNormalized();
+        return result;
+    }
+    public float GetBGMVolumeNormalized() 
+    {
+        return 1f;
+    }
+    public float GetSFXVolumeNormalized() 
+    {
+        return 1f;
+    }
+    public void LoadBGM(string bgmPath)
+    {
+        if (bGMGroup != null) return;
+
+        GameObject obj = GameResourceManager.instance.Load<GameObject>(bgmPath);
+
+        if (obj == null) return;
+
+        GameObject bgm = Instantiate(obj) as GameObject;
+
+        bgm.name = "BGMGroup";
+        bgm.transform.SetParent(transform);
+        bgm.transform.localScale = Vector3.zero;
+        bgm.transform.localPosition = Vector3.zero;
+
+        bGMGroup = bgm.GetComponent<EABGMGroup>();
+    }
+    public void Play(AudioSource source,float desiredVolume,EASOUND_TYPE type)
+    {
+        SetVolume(source, desiredVolume, type);
+        if (source != null) source.Play();
+    }
+    public void SetVolume(AudioSource source,float desiredVolume,EASOUND_TYPE type)
+    {
+        if (source != null) source.volume = GetVolume(type) * desiredVolume;
     }
 }
