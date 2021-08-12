7bf7c6bdc551d9794d2a10b8257212e1676a7553
puos <puos033@gmail.com>
2021-08-12 23:53:40 +0900

    ActorAnim수정

--- FrameWork/Mesh/EAActorAnim.cs
+++ FrameWork/Mesh/EAActorAnim.cs
@@ -16,23 +16,103 @@ public enum AnimationEventType
 
 
 // animation management class
-public abstract class EAActorAnim : StateMachineBehaviour
+public class EAActorAnim : StateMachineBehaviour
 {
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
-    }
+    Dictionary<int, AnimState> dic_animStates = new Dictionary<int, AnimState>();
 
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
-    {
-        base.OnStateEnter(animator, stateInfo, layerIndex, controller);
-    }
-
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
-    {
-        base.OnStateExit(animator, stateInfo, layerIndex, controller);
-    }
+    protected Queue<AnimState> animStateQueue = new Queue<AnimState>();
 
     //  animation event callback
+    public delegate void AnimEventCallback(EAActorAnim anim, AnimationEventType eventType, string slotName);
+
+    // animation param slot callback
+    public delegate void AnimParamSlotCallback(GameObject targetObj);
+
+    // animation information
+    [Serializable]
+    public class PlayAnimParam
+    {
+        public enum Type { Trigger, Integer }
+        public string aniName;
+        public Type type;
+        public int value;
+
+        [HideInInspector] public int paramId;
+
+        public void Initialize(){  this.paramId = Animator.StringToHash(aniName);   }
+    }
+
+    [Serializable]
+    public class AnimParamSlot
+    {
+        public string name;
+        public AnimParamSlotCallback callback;
+        public GameObject innerObj;
+    }
+
+    [Serializable]
+    public class AnimState
+    {
+        public string key;
+
+        [SerializeField]
+        public AnimParamSlot[] m_paramSlots;
+
+        [SerializeField]
+        public List<PlayAnimParam> playAnimParams = new List<PlayAnimParam>();
+
+        public void SetParam(string slotName,AnimParamSlotCallback cb)
+        {
+            int slotIdx = FindParamSlotIdxByName(slotName);
+            if(slotIdx != -1)
+            {
+                AnimParamSlot slot = m_paramSlots[slotIdx];
+                slot.callback = cb;
+                if (slot.innerObj != null) cb(slot.innerObj);
+            }
+        }
+
+        public void SetInnerObject(string slotName, GameObject obj)
+        {
+            int slotIdx = FindParamSlotIdxByName(slotName);
+            if (slotIdx != -1)
+            {
+                AnimParamSlot slot = m_paramSlots[slotIdx];
+                slot.innerObj = obj;
+            }
+        }
+
+        int FindParamSlotIdxByName(string name)
+        {
+            for (int i = 0; i < m_paramSlots.Length; i++)
+                if (m_paramSlots[i].name.Equals(name))
+                    return i;
+            return -1;
+        }
+               
+    }
+
+    [SerializeField]
+    private AnimState[] animState = null;
+
+    private Animator m_anim = null;
+
+    // Change animations and events
+    public void ResetAnimState(AnimState[] animStates)
+    {
+       
+    }
+
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        base.OnStateMachineEnter(animator, stateMachinePathHash);
+        m_anim = animator;
+        ResetAnimState(animState);
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);           
+    }
 
 }
