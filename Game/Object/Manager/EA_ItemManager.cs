ec203d81e319550ebc6de5db6228c8ff526d68c7
puos <puos033@gmail.com>
2021-08-21 14:21:35 +0900

    아이템 attach 기능

--- Game/Object/Manager/EA_ItemManager.cs
+++ Game/Object/Manager/EA_ItemManager.cs
@@ -11,15 +11,13 @@ public class EA_ItemManager : EAGenericSingleton<EA_ItemManager>
 {
     Dictionary<ITEM_UNIT_INDEX, EA_CItemUnit> m_mapItemUnitList = new Dictionary<ITEM_UNIT_INDEX, EA_CItemUnit>();
     
-    Dictionary<EAObjID, EA_Equipment> m_PCEquipMap = new Dictionary<EAObjID, EA_Equipment>(); //  [4/7/2014 puos] npc and mob item management
-    EA_Equipment m_pMyEquipment = new EA_Equipment();  //  [4/7/2014 puos] My Item Management
-
+    Dictionary<EAObjID, EA_Equipment> m_PCEquipMap = new Dictionary<EAObjID, EA_Equipment>(); //  [4/7/2014 puos] all actor item management
+    
     //	unique index for create and release
     EAIDGenerator m_IDGenerater = new EAIDGenerator(10000);
 
     public void Destroy()
     {
-        m_pMyEquipment.RemoveAllItem();
         m_IDGenerater.ReGenerate();
         var it = m_PCEquipMap.GetEnumerator();
         while(it.MoveNext()) 
@@ -29,13 +27,34 @@ public class EA_ItemManager : EAGenericSingleton<EA_ItemManager>
         m_PCEquipMap.Clear();
     }
     // Create the corresponding actor equipment item with id.
-    public bool InsertPCEquip(EAObjID id, EA_Equipment pPLEquip)
+    private bool InsertPCEquip(EAObjID id, EA_Equipment pPLEquip)
     {
         if (m_PCEquipMap.TryGetValue(id, out EA_Equipment pEquipment)) return false;
 
         m_PCEquipMap.Add(id, pPLEquip);
         return true;
     }
+
+    // Expose InsertPCEquip to the outside.
+    public bool InsertEquip(EAObjID id,uint slot,EA_CItemUnit pItem)
+    {
+        EA_CCharBPlayer pCharBase = EACObjManager.instance.GetActor(id);
+        if (pCharBase == null) return false;
+
+        EA_Equipment pEquipment = GetPCEquipItem(id);
+         
+        if(pEquipment == null)
+        {
+            pEquipment = new EA_Equipment();
+            InsertPCEquip(id, pEquipment);
+        }
+
+        //  [4/10/2014 puos] Equipped with items
+        pEquipment.InsertEquipItem(slot, pItem);
+        
+        return true;
+    }
+
     // Delete the PCEquip by id.
     public bool RemovePCEquip(EAObjID id)
     {
@@ -46,16 +65,7 @@ public class EA_ItemManager : EAGenericSingleton<EA_ItemManager>
         return true;
     }
     // Find the player equipment by id
-    public EA_Equipment GetEquipment(EAObjID id)
-    {
-        EA_CCharBPlayer pCharBase = EACObjManager.instance.GetActor(id);
-        if (pCharBase == null) return new EA_Equipment();
-        if (pCharBase.GetObjInfo().m_eObjType == eObjectType.CT_MYPLAYER) return m_pMyEquipment;
-
-        return GetPCEquipItem(id);
-    }
-    
-    private EA_Equipment GetPCEquipItem(EAObjID id)
+    public EA_Equipment GetPCEquipItem(EAObjID id)
     {
         m_PCEquipMap.TryGetValue(id, out EA_Equipment pEquipment);
         return pEquipment;
@@ -63,14 +73,11 @@ public class EA_ItemManager : EAGenericSingleton<EA_ItemManager>
     public bool RemoveEquip(EAObjID id)
     {
         EA_CCharBPlayer pCharBase = EACObjManager.instance.GetActor(id);
-        
         if (pCharBase == null) return false;
-        if (pCharBase.GetObjInfo().m_eObjType == eObjectType.CT_MYPLAYER) return false;
-        
         RemovePCEquip(id);
         return true;
     }
-    EA_CItemUnit CreateItemUnit(EAItemInfo info)
+    public EA_CItemUnit CreateItemUnit(EAItemInfo info)
     {
         if (CObjGlobal.InvalidItemID == info.m_EAItemId) info.m_EAItemId = (EAObjID)m_IDGenerater.GenerateID();
 
@@ -97,5 +104,14 @@ public class EA_ItemManager : EAGenericSingleton<EA_ItemManager>
     {
         m_mapItemUnitList.TryGetValue(id, out EA_CItemUnit itemUnit);
         return itemUnit;
+    }
+    
+    public bool EquipmentItem(EAObjID id,uint equip_slot)
+    {
+        EA_CCharBPlayer pUser = EACObjManager.instance.GetActor(id);
+        if (pUser == null) return false;
+        EA_Equipment pEquipment = GetPCEquipItem(id);
+        pEquipment.EquipItem(pUser, equip_slot);
+        return true;
     }
 }
