570bdbacb038f21f573ccef6a3ce0d229f831ac1
puos033 <puos033@snowpipe.co.kr>
2021-09-02 16:40:24 +0900

    Sfx 기능 추가

---
+++ Assets/Plugins/EAProjectV3/FrameWork/Sfx/SfxNotiReceiver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class SfxNotiReceiver : MonoBehaviour, INotificationReceiver
+{
+    private EASfx sfx = null;
+    public void Initialize(EASfx sfx)
+    {
+        this.sfx = sfx;
+    }
+    public void OnNotify(Playable origin, INotification notification, object context)
+    {
+        if (notification is SfxSignalMarker marker)
+        {
+            sfx.AnimEvent_Impact(marker.Param);
+        }
+    }
+}
---
+++ Assets/Plugins/EAProjectV3/FrameWork/Sfx/SfxSignalTrack.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.Timeline;
+
+[TrackBindingType(typeof(SfxNotiReceiver))]
+[TrackColor(255f / 255f, 140f / 255f, 0f / 255f)]
+public class SfxSignalTrack : MarkerTrack
+{
+}
