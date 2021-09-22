de078c8834c14a6d8d1b9a100398b98ab96b85d3
puos <puos033@gmail.com>
2021-09-22 23:01:57 +0900

    ActorAnim

---
+++ FrameWork/Mesh/EAAniStateBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EAAniStateBehaviour : StateMachineBehaviour
+{
+    private EAActorAnim actorAnim = null;
+
+    public void Init(EAActorAnim actorAnim)
+    {
+        this.actorAnim = actorAnim;
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (actorAnim != null) actorAnim.StateEvent_Impact($"{stateInfo.fullPathHash}", "enter");
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (actorAnim != null) actorAnim.StateEvent_Impact($"{stateInfo.fullPathHash}", "exit");
+    }
+}
