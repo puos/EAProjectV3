3b519a118ba444581b06a5975956d107f49b4cb1
puos033 <puos033@snowpipe.co.kr>
2021-10-12 16:01:47 +0900

    다국어 지원 작업

--- Editor/MultiLanguageTextEditor.cs
+++ Editor/MultiLanguageTextEditor.cs
@@ -35,6 +35,73 @@ public class MultiLanguageTextEditor : Editor
     public override void OnInspectorGUI()
     {
         EditorGUILayout.LabelField("Multi Language", EditorStyles.boldLabel);
+        EditorGUI.BeginChangeCheck();
+
+        curtarget.uiID = EditorGUILayout.TextField("UI ID", curtarget.uiID);
+        curtarget.langType = (LANGUAGE_TYPE)EditorGUILayout.EnumPopup("language:", curtarget.langType);
+        curtarget.uiType = (UI_TEXT_TYPE)EditorGUILayout.EnumPopup("type:", curtarget.uiType);
+        isSnapFoldShow = EditorGUILayout.Foldout(isSnapFoldShow, "snapshot");
+
+        bool isChangeSnapShow = false;
+        bool isDelete = false;
+
+        if(isSnapFoldShow)
+        {
+            LANGUAGE_TYPE deleteLang = LANGUAGE_TYPE.KOREAN;
+           
+            var it = curtarget.posSnaps.GetEnumerator();
+
+            while(it.MoveNext())
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.SelectableLabel(it.Current.Key.ToString());
+                if(GUILayout.Button("-"))
+                {
+                    isDelete = true;
+                    deleteLang = it.Current.Key;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (isDelete)
+            {
+                curtarget.posSnaps.Remove(deleteLang);
+            }
+
+            // Buttons for setting snapshots and default positions
+            EditorGUILayout.BeginHorizontal();
+
+            if(GUILayout.Button("Snapshot"))
+            {
+                MultiLanguageText.ChangeRectTransform t = new MultiLanguageText.ChangeRectTransform();
+                t.RestoreRect(curtarget);
+                curtarget.posSnaps[curtarget.langType] = t;
+                isChangeSnapShow = true;
+            }
+            
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+
+            foreach(TransformType t in (TransformType[])Enum.GetValues(typeof(TransformType)))
+            {
+                if(GUILayout.Button(t.ToString()))
+                {
+                    curtarget.posSnaps[curtarget.langType] = new MultiLanguageText.ChangeRectTransform(transformList[(int)t]);
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if(isChangeSnapShow || isDelete)
+        {
+            curtarget.langList = new LANGUAGE_TYPE[curtarget.posSnaps.Keys.Count];
+            curtarget.posSnaps.Keys.CopyTo(curtarget.langList, 0);
+
+            curtarget.rectTransformList = new MultiLanguageText.ChangeRectTransform[curtarget.posSnaps.Values.Count];
+
+        } 
 
         base.OnInspectorGUI();
     }
