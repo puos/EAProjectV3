41af8febf8f664fee4764be43c4149dadb2c8f3a
puos <puos033@gmail.com>
2021-10-25 22:16:15 +0900

    AI 추가 작업중

---
+++ Runtime/Game/Object/AI/EAAIAgent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface EAAIAgent : EAAIObject
+{
+    uint objectId { get; set; }
+    Vector3 VTarget();
+    void SetVTarget(Vector3 vCrosshair);
+    Vector3 GetVelocity();
+    float GetMaxSpeed();
+    void SetMaxSpeed(float newSpeed);
+    float GetSpeed();
+    Vector3 GetSide();
+    EAAIGroup GetAIGroup();
+    void SetAIGroup(EAAIGroup aiGroup);
+    /**
+    * @return time elapsed from last update
+    */
+    float GetTimeElapsed();
+}
