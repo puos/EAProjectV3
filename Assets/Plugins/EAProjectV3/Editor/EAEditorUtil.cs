9b9e41758eebe118bd1c2c2f471e7f0af7fd7ead
puos033 <puos033@snowpipe.co.kr>
2021-12-01 18:36:15 +0900

    Editor 수정

--- Assets/Plugins/EAProjectV3/Editor/EAEditorUtil.cs
+++ Assets/Plugins/EAProjectV3/Editor/EAEditorUtil.cs
@@ -9,13 +9,14 @@ using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 [InitializeOnLoad]
-public static class EAEditorUtil
+public class EAEditorUtil
 {
     class Events
     {
         public float execTime;
         public Action cb;
     }
+    
     static List<Events> eventsJob = new List<Events>();
 
     static EAEditorUtil()
@@ -24,7 +25,7 @@ public static class EAEditorUtil
     }
 
     [MenuItem("Tools/Save Assets %&#s")]
-    static public void SaveAssets()
+    public static void SaveAssets()
     {
         EditorUtility.DisplayProgressBar("Notice", "Assets are being saved to disk ", 0.5f);
 
@@ -131,102 +132,6 @@ public static class EAEditorUtil
     public static bool ConfirmMsgBox(string msg)
     {
         return EditorUtility.DisplayDialog("Notice", msg, "OK", "Cancel");
-    }
-
-    public static void SetDirty(UnityEngine.Object target)
-    {
-        SetDirty(target, true);
-    }    
-    public static void SetDirty(UnityEngine.Object target,bool setSceneDirty)
-    {
-        EditorUtility.SetDirty(target);
-        Scene currScene = EditorSceneManager.GetActiveScene();
-        if (!Application.isPlaying) EditorSceneManager.MarkSceneDirty(currScene);
-    }
-
-    public static bool GetBool(string name, bool defaultValue) { return EditorPrefs.GetBool(name, defaultValue); }
-    public static void SetBool(string name, bool val) { EditorPrefs.SetBool(name, val); }
-
-    public static bool minimalisticLook
-    {
-        get { return GetBool("Minimalistic",false); }
-        set { SetBool("Minimalistic",value); }
-    }
-
-    static bool mEndHorizontal = false;
-
-    public static string textArea = "TextArea";
-
-    public static void BeginContents() { BeginContents(EAEditorUtil.minimalisticLook); }
-
-    public static void BeginContents(bool minimalistic)
-    {
-        if(!minimalistic)
-        {
-            mEndHorizontal = true;
-            GUILayout.BeginHorizontal();
-            EditorGUILayout.BeginHorizontal(textArea , GUILayout.MinHeight(10f));
-        }
-        else
-        {
-            mEndHorizontal = false;
-            EditorGUILayout.BeginHorizontal(GUILayout.MinHeight(10f));
-            GUILayout.Space(10f);
-        }
-        GUILayout.BeginVertical();
-        GUILayout.Space(2f);
-    }
-
-    // End drawing the content area.
-    public static void EndContents()
-    {
-        GUILayout.Space(3f);
-        GUILayout.EndVertical();
-        EditorGUILayout.EndHorizontal();
-        if(mEndHorizontal)
-        {
-            GUILayout.Space(3f);
-            GUILayout.EndHorizontal();
-        }
-        GUILayout.Space(3f);
-    }
-
-    public static bool DrawHeader(string text, bool detailed) { return DrawHeader(text, text, detailed, !detailed); }
-
-    public static bool DrawHeader(string text,string key,bool forceOn,bool minimalistic)
-    {
-        bool state = GetBool(key, true);
-        if (!minimalistic) GUILayout.Space(3f);
-        if (!forceOn && !state) GUI.backgroundColor = new Color(0.8f, 0.8f, 0.8f);
-        GUILayout.BeginHorizontal();
-        GUI.changed = false;
-
-        if(minimalistic)
-        {
-            if (state) text = "\u25BC" + (char)0x200a + text;
-            else text = "\u25BA" + (char)0x200a + text;
-
-            GUILayout.BeginHorizontal();
-            GUI.contentColor = EditorGUIUtility.isProSkin ? new Color(1f, 1f, 1f, 0.7f) : new Color(0f, 0f, 0f, 0.7f);
-            if (!GUILayout.Toggle(true, text, "PreToolbar2", GUILayout.MinWidth(20f))) state = !state;
-            GUI.contentColor = Color.white;
-            GUILayout.EndHorizontal();
-        }
-        else
-        {
-            text = "<b><size=11>" + text + "</size></b>";
-            if (state) text = "\u25BC " + text;
-            else text = "\u25BA " + text;
-            if (!GUILayout.Toggle(true, text,"dragtab", GUILayout.MinWidth(20f))) state = !state;
-        }
-
-        if (GUI.changed) SetBool(key, state);
-
-        if (!minimalistic) GUILayout.Space(2f);
-        GUILayout.EndHorizontal();
-        GUI.backgroundColor = Color.white;
-        if (!forceOn && !state) GUILayout.Space(3f);
-        return state;
     }
 
     static void OnEditorUpdate() 
