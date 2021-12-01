438f28e3f5c7b845134b9dfe3c60f4e1648c1b43
puos033 <puos033@snowpipe.co.kr>
2021-12-01 14:44:05 +0900

    JsonFX 수정

---
+++ Runtime/FrameWork/Util/JsonUtil.cs
@@ -0,0 +1,32 @@
+﻿using JsonFx.Json;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public static class JsonUtil
+{
+    public static string ObjToJson(object obj, bool prettyPrint = false)
+    {
+        if (prettyPrint == false) return JsonWriter.Serialize(obj);
+
+        JsonWriterSettings jwsetting = new JsonWriterSettings();
+        jwsetting.PrettyPrint = true;
+        StringBuilder sb = new StringBuilder();
+        JsonWriter writer = new JsonWriter(sb, jwsetting);
+        writer.Write(obj);
+
+        return sb.ToString();
+    }
+    public static T JsonToObj<T>(string json)
+    {
+        return JsonReader.Deserialize<T>(json);
+    }
+    public static Dictionary<string,object> JsonToDic(string json)
+    {
+        return JsonReader.Deserialize<Dictionary<string,object>>(json);
+    }
+    public static object JsonToObj(string json,System.Type type)
+    {
+        return JsonReader.Deserialize(json, type);
+    }
+}
