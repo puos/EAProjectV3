68427c108b3e761af93896458e7ddb87c07a7913
puos033 <puos033@snowpipe.co.kr>
2021-12-06 18:30:34 +0900

    sound 수정

--- Runtime/FrameWork/Sfx/EASoundManager.cs
+++ Runtime/FrameWork/Sfx/EASoundManager.cs
@@ -70,11 +70,11 @@ public class EASoundManager : Singleton<EASoundManager>
     }
     public float GetBGMVolumeNormalized() 
     {
-        return OptionManager.instance.GetValueInRatio(BASEOptions.BgmVolume.ToString(), 0,bgmMaxVolume);
+        return OptionManager.instance.GetValueInRatio(bgmVolume, 0,bgmMaxVolume);
     }
     public float GetSFXVolumeNormalized() 
     {
-        return OptionManager.instance.GetValueInRatio(BASEOptions.SfxVolume.ToString(), 0,sfxMaxVolume);
+        return OptionManager.instance.GetValueInRatio(sfxVolume, 0,sfxMaxVolume);
     }
     public void LoadBGM(string bgmPath)
     {
