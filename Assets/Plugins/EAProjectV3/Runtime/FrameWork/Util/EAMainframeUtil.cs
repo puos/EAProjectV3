042cf5513ce2cf123fb48cac81eff1b410745841
puos <puos033@gmail.com>
2021-12-29 20:16:31 +0900

    option 추가

--- Assets/Plugins/EAProjectV3/Runtime/FrameWork/Util/EAMainframeUtil.cs
+++ Assets/Plugins/EAProjectV3/Runtime/FrameWork/Util/EAMainframeUtil.cs
@@ -16,7 +16,7 @@ public class EAMainframeUtil
         EAFrameUtil.Call<UIManager>(UIManager.instance);
         EAFrameUtil.Call<EASceneLoadingManager>(EASceneLoadingManager.instance);
         EAFrameUtil.Call<EASoundManager>(EASoundManager.instance);
-        //EAFrameUtil.Call<EAOptio>
+        EAFrameUtil.Call<OptionManager>(OptionManager.instance);
         EAFrameUtil.Call<EACObjManager>(EACObjManager.instance);
         EAFrameUtil.Call<EA_ItemManager>(EA_ItemManager.instance);
         EAFrameUtil.Call<EASfxManager>(EASfxManager.instance);
