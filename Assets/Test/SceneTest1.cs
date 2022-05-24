e25d4db089e9a9fb095e5d9d464d612ab729070e
puos033 <puos033@snowpipe.co.kr>
2022-05-24 14:52:49 +0900

    씬로딩 구현중

---
+++ Assets/Test/SceneTest1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[EASceneInfo(typeof(SceneTest1))]
+#endif
+
+public class SceneTest1 : EASceneLogic
+{
+    protected override void OnClose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnInit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
