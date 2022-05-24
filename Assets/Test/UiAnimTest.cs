42164437bcc9652f5093746c6e99dc491aa8c0d8
puos033 <puos033@snowpipe.co.kr>
2022-05-24 18:20:22 +0900

    코드 추가

---
+++ Assets/Test/UiAnimTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[EASceneInfo(typeof(UiAnimTest))]
+#endif
+
+public class UiAnimTest : EASceneLogic
+{
+    protected override void OnClose()
+    {
+       
+    }
+
+    protected override void OnInit()
+    {
+       
+    }
+
+    protected override void OnUpdate()
+    {
+       
+    }
+}
