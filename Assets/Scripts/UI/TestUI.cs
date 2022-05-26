b716aeabd6336d0f11a2f60f66c2aa92724af83d
puos033 <puos033@snowpipe.co.kr>
2022-05-26 14:16:30 +0900

    코드 수정

--- Assets/Scripts/UI/TestUI.cs
+++ Assets/Scripts/UI/TestUI.cs
@@ -5,22 +5,54 @@ using UnityEngine;
 public class TestUI : UICtrl
 {
     [SerializeField] private UITween_TweenRuntime m_Tween = null;
+    [SerializeField] private UITween_TweenRuntime m_Tween2 = null;
+    [SerializeField] private UITween_TweenRuntime m_Tween3 = null;
 
     public override void Initialize()
     {
         base.Initialize();
         m_Tween.enabled = false;
+        m_Tween2.enabled = false;
+        m_Tween3.enabled = false;
     }
 
     public void OnClickEvent()
     {
-        Debug.Log("TestUI OnClickEvent");
+       Debug.Log("TestUI OnClickEvent");
         m_Tween.enabled = true;
         m_Tween.Play();
+    }
+
+    public void OnClickEvent2() 
+    {
+        if (m_Tween2.isPlaying) return;
+        
+        Debug.Log("TestUI OnClickEvent2");
+        m_Tween2.enabled = true;
+        m_Tween2.Play();
+    }
+
+    public void OnClickEvent3()
+    {
+        if (m_Tween3.isPlaying) return;
+
+        Debug.Log("TestUI OnClickEvent3");
+        m_Tween3.enabled = true;
+        m_Tween3.Play();
     }
 
     public void OnClickComplete()
     {
         Debug.Log("TestUI OnClickComplete");
+    }
+
+    public void OnClickComponent2()
+    {
+        Debug.Log("TestUI OnClickComplete2");
+    }
+
+    public void OnClickComponent3()
+    {
+        Debug.Log("TestUI OnClickComplete3");
     }
 }
